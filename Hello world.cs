4051a31b386a0d0a3a63862feea32bc3d219bce1
RavenLoyds <67434496+RavenX-13@users.noreply.github.com>
2023-02-02 20:36:32 +0100

    Update Hello world.cs

--- Hello world.cs
+++ Hello world.cs
@@ -6,11 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-			/* This command writes Hello World on the screen*/
+			// This command writes Hello World on the screen
             Console.WriteLine("Hello, World!");
-			/* Console.Readline reads the screen for user input.*/
+			// Console.Readline reads the screen for user input.
             Console.ReadLine();
-			/* dont forget to write ";" on the end of most commands */
+			// dont forget to write ";" on the end of most commands 
         }
     }
 }
